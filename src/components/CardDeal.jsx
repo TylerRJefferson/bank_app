aff72f0870cdf51cb3fd1aa36bca3294e9b0c6a9
Tyler Jefferson <tylerrjefferson00@gmail.com>
2023-02-09 13:30:53 -0500

    added button w rounding

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -7,6 +7,8 @@ const CardDeal = () => (
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" /> in a few easy steps.</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
+      
+      <Button styles="mt-10" />
     </div>
   </section>
 );
